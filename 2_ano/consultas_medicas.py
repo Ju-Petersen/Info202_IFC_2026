14dc827dcf862e8103e70e7c388166c741b3d9a3
Ju.Petersen <juliapetersenavi@gmail.com>
2026-03-17 12:29:41 +0000

    exames

--- 2_ano/consultas_medicas.py
+++ 2_ano/consultas_medicas.py
@@ -3,38 +3,41 @@ class Medico:
         self.nome_medico = nm_m
         self.especialidade = esp
     def __str__(self):
-        return f'''
-                Médico: {self.nome_medico}
-                Especialidade: {self.especialidade}
+        return f'''{self.nome_medico}, {self.especialidade}
 '''
 
 #O modo str deve dar print das informações necessária no teste
 class Paciente:
-    def __init__(self, nm, em, dt_nasc):
+    def __init__(self, nm, em, dt_nasc, med, ex):
         self.nome = nm
         self.email = em
         self.data_nascimento = dt_nasc
+        self.medico = med
+        self.exame = ex
     def __str__(self):
         return f'''
                 Nome do Paciente: {self.nome}
                 Email: {self.email}
                 Data de nascimento: {self.data_nascimento}
+                Médico que realixou o atendimento: {self.medico}
+                Exame realizado: {self.exame}
 '''
 class Exame:
-    def __init__(self, dt_r, rs, dh_c):
-        self.data_realizado = dt_r
+    def __init__(self, dt_rel, dthr_cons, rs, nm_ex):
+        self.data_realizado = dt_rel
         self.resultado = rs
-        self.datahora_consulta = dh_c
+        self.datahora_consulta = dthr_cons
+        self.nome_exame = nm_ex
     def __str__(self):
-        return f'''
+        return f'''{self.nome_exame}
                 Data do Exame: {self.data_realizado}
                 Informações da consulta: {self.datahora_consulta}
                 Resultado: {self.resultado}
 '''
 #classes para armazenar informações da consulta, abaixo teste das classes
 
-m1 = Medico("Pedro", "Radiologista")
-p1 = Paciente("Maria", "ma@gmail.com", "24/12/1977")
-c1 = Exame("23/03/26", "fratura na costela", "24/03/26 as 13:30")
+m1 = Medico("Pedro", "Radiologia")
+e1 = Exame("03/03/2026", "04/03/2026", "fratura no punho", "Raio-x")
+p1 = Paciente("Maria", "ma@gmail.com", "24/03/1999",m1, e1)
 
-print(m1, p1, c1)
+print(p1)

14e283c2f1f9d52b9233f19dd5b9f41b8bd3265a
Ju.Petersen <juliapetersenavi@gmail.com>
2026-03-24 12:29:45 +0000

    exame

--- 2_ano/consultas_medicas.py
+++ 2_ano/consultas_medicas.py
@@ -8,36 +8,41 @@ class Medico:
 
 #O modo str deve dar print das informações necessária no teste
 class Paciente:
-    def __init__(self, nm, em, dt_nasc, med, ex):
+    def __init__(self, nm, em, dt_nasc, med):
         self.nome = nm
         self.email = em
         self.data_nascimento = dt_nasc
         self.medico = med
-        self.exame = ex
     def __str__(self):
-        return f'''
-                Nome do Paciente: {self.nome}
+        return f'''{self.nome}
                 Email: {self.email}
                 Data de nascimento: {self.data_nascimento}
                 Médico que realixou o atendimento: {self.medico}
-                Exame realizado: {self.exame}
 '''
 class Exame:
-    def __init__(self, dt_rel, dthr_cons, rs, nm_ex):
+    def __init__(self, dt_rel, dthr_cons, rs, nm_ex, paci):
         self.data_realizado = dt_rel
         self.resultado = rs
         self.datahora_consulta = dthr_cons
         self.nome_exame = nm_ex
+        self.paciente = paci
     def __str__(self):
-        return f'''{self.nome_exame}
+        return f'''
+                Exame realizado: {self.nome_exame}
                 Data do Exame: {self.data_realizado}
                 Informações da consulta: {self.datahora_consulta}
                 Resultado: {self.resultado}
+                
+                Paciente: {self.paciente}
 '''
 #classes para armazenar informações da consulta, abaixo teste das classes
 
 m1 = Medico("Pedro", "Radiologia")
-e1 = Exame("03/03/2026", "04/03/2026", "fratura no punho", "Raio-x")
-p1 = Paciente("Maria", "ma@gmail.com", "24/03/1999",m1, e1)
+p1 = Paciente("Maria", "ma@gmail.com", "24/03/1999", m1)
+e1 = Exame("03/03/2026", "04/03/2026", "fratura no punho", "Raio-x", p1)
+e2 = Exame("04/03/2026", "04/03/2026", "vbnhufgbvyfd", "A", p1)
 
-print(p1)
+print(f'''
+{e1}
+{e2}
+''')

a56b0a3ec40a284dfd50d4af6791da68a8899ce6
Ju.Petersen <juliapetersenavi@gmail.com>
2026-04-01 12:18:44 +0000

    sitema de oficina mecânica feita a partir dos diagramas do professor.

--- 2_ano/oficina_mecanica.py
+++ 2_ano/oficina_mecanica.py
@@ -1,3 +1,4 @@
+#Definição das classes
 class Pessoa():
     def __init__(self, nm, telef, dt_nasc):
         self.nome = nm
@@ -44,6 +45,7 @@ class Servico_realizado():
     def __str__(self):
         return f'''
         {self.servico}
+        Realizado por:
         {self.mecanico}
         '''
         
@@ -58,15 +60,15 @@ class Ordem_servico():
     def __str__(self):
         return f'''
         Data que o veículo entrou: {self.data_entrada}
+        Data que o veículo saiu: {self.data_saida}
+        Desconto aplicado: {self.desconto}
+
         Descrição do veículo: 
         {self.veiculo}
-        Data que o veículo saiu: {self.data_saida}
-
         Dados do cliente:
         {self.cliente}
-        Desconto aplicado: {self.desconto}
         '''
-        
+
 class Veiculo():
     def __init__(self, placa, cor):
         self.placa = placa
@@ -74,48 +76,52 @@ class Veiculo():
 
     def __str__(self):
         return f'''{self.placa}
-        cor: {self.cor}'''
+            cor: {self.cor}'''
         
-class Moto(Veiculo):
+class Moto(Veiculo): #Sendo "Moto", filho de "Veiculo".
     def __init__(self, placa, cor):
-        super().__init__(placa, cor)
+        super().__init__(placa, cor) #Herda apenas placa e cor (no exercício não foi pedido que adicionasse a diferenciação por cilindradas).
 
     def __str__(self):
         return f'''{super().__str__()}
         '''
+#Classes com herança:
 
-class VeiculoComPassageiro(Veiculo):
+class VeiculoComPassageiro(Veiculo): #Sendo "VeiculoComPassageiro", filho de "Veiculo".
     def __init__(self, placa, cor, lugares):
-        super().__init__(placa, cor)
+        super().__init__(placa, cor) #Herda placa e cor, e por ter passageiros é adicionado "lugares".
         self.lugares = lugares
 
     def __str__(self):
         return f'''{super().__str__()}
-        lugares do veículo: {self.lugares}'''
+            lugares do veículo: {self.lugares}'''
         
-class Carro(VeiculoComPassageiro):
+class Carro(VeiculoComPassageiro): #Sendo "Carro", filho de "VeiculoComPassageiro".
     def __init__(self, placa, lugares, cor, portas):
-        super().__init__(placa, cor, lugares)
+        super().__init__(placa, cor, lugares) #Herda placa, cor, e lugares.Por ter diferenciação de portas, "portas" é adicionado.
         self.portas = portas
 
     def __str__(self):
         return f'''placa: {super().__str__()}
-        portas do veículo: {self.portas}'''
+            portas do veículo: {self.portas}'''
 
-class Onibus(VeiculoComPassageiro):
+class Onibus(VeiculoComPassageiro): #Sendo "Carro", filho de "VeiculoComPassageiro".
     def __init__(self, placa, cor, lugares):
-        super().__init__(placa, cor, lugares)
+        super().__init__(placa, cor, lugares) #Herda placa, cor, e lugares.
 
     def __str__(self):
         return f'''placa: {super().__str__()}'''
         
 pess = Pessoa("João", "(47)123456", "28/01/2010")
 pess1 = Pessoa("Mario", "(47)654321", "02/12/2009")
-mec = Mecanico(pess1)
-cli = Cliente(pess, "joaoo@gmail.com")
-vei = Carro("AAAAAAAA", 5, "azul", 4)
-ser = Servico("troca de pneu", "R$250")
-serre = Servico_realizado(ser, mec)
-ord_ser = Ordem_servico("01/04/2026", vei, "26/04/2026", cli, 20)
+mec = Mecanico(pess1) #Mecânico é uma pessoa.
+cli = Cliente(pess, "joaoo@gmail.com") #Cliente é uma pessoa + email desta.
+vei = Carro("AAAAAAAA", 5, "azul", 4) #Lembrar que carro é um veículo, ou que "vei" é igual a "Carro(...)".
+ser = Servico("troca de pneu", f"R${250}")
+serre = Servico_realizado(ser, mec) #O serviço é realizado ao ter um mecânico designado e um "tipo" de serviço.
+ord_ser = Ordem_servico("31/03/2026", vei, "01/04/2026", cli, f"{20}%")
 
+print(serre)
 print(ord_ser)
+
+#Implementar soma de valores e dscontos (com tabela de registros?).

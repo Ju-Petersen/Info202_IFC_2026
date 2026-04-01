ad1183e5ebda329a21b66c7411db8bb29de00837
Ju.Petersen <juliapetersenavi@gmail.com>
2026-04-01 11:09:34 +0000

    oficina mecânica, desenvolvido a partir de diagramas

--- 2_ano/oficina_mecanica.py
+++ 2_ano/oficina_mecanica.py
@@ -4,24 +4,44 @@ class Pessoa():
         self.telefone = telefone
         self.data_nascimento = data_nascimento
 
+    def __str__(self):
+        return f'''
+        '''
+
 class Mecanico():
     def __init__(self, pessoa):
         self.pessoa =  pessoa
+
+    def __str__(self):
+        return f'''
+        '''
 
 class Cliente():
     def __init__(self, pessoa, email):
         self.pessoa = pessoa
         self.email = email
+
+    def __str__(self):
+        return f'''
+        '''
         
 class Servico():
     def __init__(self, descricao, valor):
         self.descricao = descricao
         self.valor = valor
+
+    def __str__(self):
+        return f'''
+        '''
         
 class Servico_realizado():
     def __init__(self, servico, mecanico):
         self.servico = servico
         self.mecanico = mecanico
+
+    def __str__(self):
+        return f'''
+        '''
         
 class Ordem_servico():
     def __init__(self, data_entrada, veiculo, data_saida, cliente, desconto):
@@ -30,29 +50,57 @@ class Ordem_servico():
         self.data_saida = data_saida
         self.cliente = cliente
         self.desconto = desconto
+
+    def __str__(self):
+        return f'''
+        '''
         
 class Veiculo():
     def __init__(self, placa, cor):
         self.placa = placa
         self.cor = cor
+
+    def __str__(self):
+        return f'''
+        '''
         
 class Moto(Veiculo):
     def __init__(self, placa, cor):
         super().__init__(placa, cor)
 
+    def __str__(self):
+        return f'''
+        {super().__str__()}
+        '''
+
 class VeiculoComPassageiro(Veiculo):
     def __init__(self, placa, cor, lugares):
         super().__init__(placa, cor)
         self.lugares = lugares
+
+    def __str__(self):
+        return f'''
+        {super().__str__()}
+        '''
         
 class Carro(VeiculoComPassageiro):
     def __init__(self, placa, cor, lugares, portas):
         super().__init__(placa, cor, lugares)
         self.portas = portas
 
+    def __str__(self):
+        return f'''
+        {super().__str__()}
+        '''
+
 class Onibus(VeiculoComPassageiro):
     def __init__(self, placa, cor, lugares):
         super().__init__(placa, cor, lugares)
+
+    def __str__(self):
+        return f'''
+        {super().__str__()}
+        '''
         
 pess1 = Pessoa("João", "(47)123456", "28/01/2010")
 pess2 = Pessoa("Mario", "(47)654321", "02/12/2009")

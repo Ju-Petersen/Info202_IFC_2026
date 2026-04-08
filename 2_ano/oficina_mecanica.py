252bf61ddc5ece9f07cf9d5402427f0149d5c5dd
Ju.Petersen <juliapetersenavi@gmail.com>
2026-04-08 11:08:54 +0000

    sistema de oficina mecanica (realizados testes com unittest)

--- 2_ano/oficina_mecanica.py
+++ 2_ano/oficina_mecanica.py
@@ -38,12 +38,14 @@ class Servico():
         '''
         
 class Servico_realizado():
-    def __init__(self, ser, mec):
+    def __init__(self, ser, mec, ord_ser):
         self.servico = ser
         self.mecanico = mec
+        self.ordem_servico = ord_ser
 
     def __str__(self):
         return f'''
+        {self.ordem_servico}
         {self.servico}
         Realizado por:
         {self.mecanico}
@@ -112,17 +114,18 @@ class Onibus(VeiculoComPassageiro): #Sendo "Carro", filho de "VeiculoComPassagei
     def __str__(self):
         return f'''placa: {super().__str__()}'''
 
-#teste de classes:
-'''
+#teste de classes (feito em test_oficina_mecanica):
+
 pess = Pessoa("João", "(47)123456", "28/01/2010")
 pess1 = Pessoa("Mario", "(47)654321", "02/12/2009")
 mec = Mecanico(pess1) #Mecânico é uma pessoa!!
 cli = Cliente(pess, "joaoo@gmail.com") #Cliente é uma pessoa + email desta.
 vei = Carro("AAAAAAAA", 5, "azul", 4) #Lembrar que carro é um veículo, ou que "vei" é igual a "Carro(...)".
 ser = Servico("troca de pneu", f"R${250}")
-serre = Servico_realizado(ser, mec) #O serviço é realizado ao ter um mecânico designado e um "tipo" de serviço.
-ord_ser = Ordem_servico("31/03/2026", vei, "01/04/2026", cli, f"{20}%")
+ord_ser1 = Ordem_servico("31/03/2026", vei, "01/04/2026", cli, f"{20}%")
+serre = Servico_realizado(ser, mec, ord_ser1) #O serviço é realizado ao ter um mecânico designado e um "tipo" de serviço.
 
-print(f"{serre} {ord_ser}")
-'''
+print(serre)
+
 #Implementar soma de valores e dscontos (com tabela de registros?).
+

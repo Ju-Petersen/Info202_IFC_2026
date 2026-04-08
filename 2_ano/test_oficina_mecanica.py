b8e9c8dce64d75f21687aaa9a127ed3aa03109f5
Ju.Petersen <juliapetersenavi@gmail.com>
2026-04-08 11:22:21 +0000

    sistema de oficina mecanica (realizados testes com unittest)

--- 2_ano/test_oficina_mecanica.py
+++ 2_ano/test_oficina_mecanica.py
@@ -6,12 +6,16 @@ from oficina_mecanica import *
 class TestOficina(unittest.TestCase):
 
     def test_pessoa(self):
-        pess = Pessoa("João", "(47)123456", "28/01/2010")
-        self.assertTrue(pess, Pessoa("João", "(47)123456", "28/01/2010"))
+        obj = Pessoa("João", "(47)123456", "28/01/2010")
+        self.assertEqual(obj.nome, "João")
+        self.assertEqual(obj.telefone, "(47)123456")
+        self.assertEqual(obj.data_nascimento, "28/01/2010")
 
-    def test_pess1(self):
-        pess1 = Pessoa("Mario", "(47)654321", "02/12/2009")
-        self.assertEqual(pess1, Pessoa("João", "(47)123456", "28/01/2010"))
+    def test_pessoa1(self):
+        obj1 = Pessoa("João", "(47)123456", "28/01/2010")
+        self.assertEqual(obj1.nome, "-1")
+        self.assertEqual(obj1.telefone, "ABCDE")
+        self.assertEqual(obj1.data_nascimento, "-0/-10/-2919")
 
 if __name__ == '__main__':
     # executa os testes

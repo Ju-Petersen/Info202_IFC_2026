0ba589b09b81ac3236602f53f26532a2cc3215b3
Ju-Petersen <juliapetersenavi@gmail.com>
2026-09-16 12:10:10 -0300

    feat:finished 'receitas.py' implementation with sqlalchemy.

--- 2_ano/python/alchemy_exercicios/receitas_db/receitas.py
+++ 2_ano/python/alchemy_exercicios/receitas_db/receitas.py
@@ -1,5 +1,6 @@
-from sqlalchemy import String, Integer
-from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column # classes e outros módulos para criação das tabelas
+from sqlalchemy import create_engine, String, Text, Integer
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, Session # classes e outros módulos para criação das tabelas
+from typing import Optional
 
 class Base(DeclarativeBase):
     pass
@@ -12,3 +13,27 @@ class Receita(Base):
     tempo_preparo: Mapped[int] = mapped_column(Integer)
     modo_preparo: Mapped[str] = mapped_column(String(500))
     ingredientes: Mapped[list] = mapped_column(String(500))
+
+engine = create_engine("mysql+pymysql://root:root@localhost:3306/receitas_db_julia")
+
+Base.metadata.create_all(engine)
+
+#Abrir sessão:
+with Session(engine) as session:
+    r1 = Receita(nome = "Bolo de milho", tempo_preparo = 50,
+
+    modo_preparo = "Bate no liquidificador a farinha, o milho, "+\
+    "o leite, óleo e os ovos, até moer bem "+\
+    "o milho. Acrescente o fermento e "+\
+    "pulse o liquidificador 3 vezes. "+\
+    "Despeje na forma e leve a forno"+\
+    " por 50 minutos. Espere esfriar e sirva.",
+
+    ingredientes = "1 lata de milho,  leite (medida da lata), " +\
+    "açúcar (medida da lata), 3 ovos, 1 colher de fermento," +\
+    "1/2 lata de óleo")
+    # adicionar o objeto ao db
+    session.add(r1)
+    session.commit() # atualizar as adições
+
+    print("Banco de dados criado (se não existia), tabela criada (se não havia) e dados inseridos no banco")
